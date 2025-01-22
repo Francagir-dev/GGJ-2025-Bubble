c85a85d91aef08f0deb170494108be738deebdd1
Francisco Carrasco Girbés <francagir@hotmail.com>
2025-01-22 11:01:38 +0100

    Add Constants
    For names, and stuff wont change

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants 
+{
+    public const string MAINSCENE_NAME = "main";
+    public const string MAINMENU_NAME = "Main Menu";
+}
